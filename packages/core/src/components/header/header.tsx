46c0e677c54d913a9b5658f332373f96ad7ee9a6
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-10-05 17:28:36 +0200

    reste le ferme

--- packages/core/src/components/header/header.tsx
+++ packages/core/src/components/header/header.tsx
@@ -1,4 +1,4 @@
-import { Component, h } from '@stencil/core';
+import { Component, h, State } from '@stencil/core';
 
 @Component({
   tag: 'fr-header',
@@ -6,6 +6,10 @@ import { Component, h } from '@stencil/core';
   shadow: true,
 })
 export class Header {
+  @State() opened = false;
+  open() {
+    this.opened = !this.opened;
+  }
   render() {
     return (
       <header role="banner" class="fr-header">
@@ -20,7 +24,7 @@ export class Header {
                     </a>
                   </div>
                   <div class="fr-header__navbar">
-                    <button class="fr-btn--menu fr-btn" data-fr-opened="false" aria-controls="modal-626" aria-haspopup="menu" id="button-627" title="Menu">
+                    <button class="fr-btn--menu fr-btn" aria-controls="modal-626" aria-haspopup="menu" id="button-627" title="Menu" onClick={() => this.open()}>
                       Menu
                     </button>
                   </div>
@@ -32,9 +36,16 @@ export class Header {
             </div>
           </div>
         </div>
-        <div class="fr-header__menu fr-modal" id="modal-626" aria-labelledby="button-627">
+        <div
+          class={{
+            'fr-header__menu fr-modal': true,
+            [`fr-modal--opened`]: this.opened,
+          }}
+          id="modal-626"
+          aria-labelledby="button-627"
+        >
           <div class="fr-container">
-            <button class="fr-btn--close fr-btn" aria-controls="modal-626" title="Fermer">
+            <button class="fr-btn--close fr-btn" aria-controls="modal-626" title="Fermer" onClick={() => this.open()}>
               Fermer
             </button>
             <div class="fr-header__menu-links"></div>
