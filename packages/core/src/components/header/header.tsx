65c5c4ac67b5c83f315a5263d009e3323b0f64ce
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-10-06 16:34:56 +0200

    link

--- packages/core/src/components/header/header.tsx
+++ packages/core/src/components/header/header.tsx
@@ -1,14 +1,32 @@
-import { Component, h, State } from '@stencil/core';
+import { Component, h, State, Element, ComponentInterface } from '@stencil/core';
 
 @Component({
   tag: 'fr-header',
   styleUrl: 'header.scss',
   shadow: true,
 })
-export class Header {
+export class Header implements ComponentInterface {
+  @Element() el!: HTMLElement;
+
   @State() opened = false;
-  close() {
-    this.opened = false;
+  open() {
+    this.opened = !this.opened;
+  }
+
+  templateService: HTMLElement;
+
+  handleClick() {
+    (this.el.querySelector('[slot=band]') as HTMLLinkElement).click();
+  }
+
+  componentWillLoad() {
+    if (this.el.querySelector('[slot=service]')) {
+      this.templateService = (
+        <div class="fr-header__service" onClick={() => this.handleClick()}>
+          <slot name="service"></slot>
+        </div>
+      );
+    }
   }
 
   render() {
@@ -17,7 +35,19 @@ export class Header {
         <div class="fr-header__body">
           <div class="fr-container">
             <div class="fr-header__body-row">
-              <slot></slot>
+              <div class="fr-header__brand fr-enlarge-link">
+                <div class="fr-header__brand-top">
+                  <div class="fr-header__logo">
+                    <slot name="band"></slot>
+                  </div>
+                  <div class="fr-header__navbar">
+                    <button class="fr-btn--menu fr-btn" aria-controls="modal-626" aria-haspopup="menu" id="button-627" title="Menu" onClick={() => this.open()}>
+                      Menu
+                    </button>
+                  </div>
+                </div>
+                {this.templateService}
+              </div>
             </div>
           </div>
         </div>
@@ -30,11 +60,11 @@ export class Header {
           aria-labelledby="button-627"
         >
           <div class="fr-container">
-            <button class="fr-btn--close fr-btn" aria-controls="modal-626" title="Fermer" onClick={() => this.close()}>
+            <button class="fr-btn--close fr-btn" aria-controls="modal-626" title="Fermer" onClick={() => this.open()}>
               Fermer
             </button>
             <div class="fr-header__menu-links"></div>
-            <slot name="navigation"></slot>
+            <slot></slot>
           </div>
         </div>
       </header>
