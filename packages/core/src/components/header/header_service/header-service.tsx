0d7cb92f097792d8111ed139362468f4bf2c0851
heihong-dgfip <hei-hong.traing@dgfip.finances.gouv.fr>
2022-10-06 16:40:30 +0200

    service

--- packages/core/src/components/header/header_service/header-service.tsx
+++ packages/core/src/components/header/header_service/header-service.tsx
@@ -6,28 +6,11 @@ import { Component, h, Element, Prop } from '@stencil/core';
   shadow: true,
 })
 export class Logo {
-  @Element() el!: HTMLElement;
-
-  /** Niveau du titre pour générer le tag HTML et conserver la sématique (optionnel) */
-  @Prop({ reflect: true }) headingLevel?: 1 | 2 | 3 | 4 | 5 | 6;
-
-  private get headingLevelFormat() {
-    return 'h' + this.headingLevel;
-  }
-
   render() {
-    let template = (
+    return (
       <p class="fr-header__service-title">
         <slot></slot>
       </p>
     );
-    if (this.headingLevel) {
-      template = (
-        <this.headingLevelFormat class="fr-header__service-title">
-          <slot></slot>
-        </this.headingLevelFormat>
-      );
-    }
-    return <div class="fr-header__service">{template}</div>;
   }
 }
